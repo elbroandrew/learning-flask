9ecf5e175b443d9319f7609830a2ae2b9fba7788
andrew <elbroandrew@gmail.com>
2021-12-17 19:35:38 +1100

    feat: add basic pages

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "<h1>Hello, World!</h1>"
+
+
+@app.route('/info')
+def info():
+    return "<h1>Puppies are cute</h1>"
+
+if __name__ == '__main__':
+    app.run()
