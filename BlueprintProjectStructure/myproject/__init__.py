58fcac0f91c5b77ade8b491b111ef43eca96b1f6
Andrew <elbroandrew@gmail.com>
2022-06-17 09:54:32 +1000

    register blueprints

--- BlueprintProjectStructure/myproject/__init__.py
+++ BlueprintProjectStructure/myproject/__init__.py
@@ -15,3 +15,10 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 Migrate(app, db)
+
+# after DB defined
+from myproject.puppies.views import puppies_blueprint
+from myproject.owners.views import owners_blueprint
+
+app.register_blueprint(owners_blueprint, url_prefix='/owners')
+app.register_blueprint(puppies_blueprint, url_prefix='/puppies')
