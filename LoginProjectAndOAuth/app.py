2ffed43b4a1dab169b5bce4635cff890181a798e
Andrew <elbroandrew@gmail.com>
2022-06-17 16:01:18 +1000

    add views

---
+++ LoginProjectAndOAuth/app.py
@@ -0,0 +1,56 @@
+from myproject import app, db
+from flask import render_template, redirect, request, url_for, flash, abort
+from flask_login import login_user, login_required, logout_user
+from myproject.models import User
+from myproject.forms import LoginForm, RegistrationForm
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/welcome')
+@login_required
+def welcome_user():
+    return render_template('welcome_user.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("You logged out!")
+    return redirect(url_for('home'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+
+        if user.check_password(form.password.data) and user is not None:
+            login_user(user)
+            flash("Logged in successfully!")
+
+            next = request.args.get('next')  # 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
