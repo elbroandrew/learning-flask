d1b92432f93bb878e5d10e4be52e49b6f55fb1ff
andrew <elbroandrew@gmail.com>
2022-07-06 19:18:19 +1100

    add user model

--- BlogPostCompanyProject/puppycompanyblog/__init__.py
+++ BlogPostCompanyProject/puppycompanyblog/__init__.py
@@ -1,11 +1,24 @@
+from puppycompanyblog.error_pages.handlers import error_pages
+from puppycompanyblog.core.views import core
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
+import os
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
 
-from puppycompanyblog.core.views import core
-from puppycompanyblog.error_pages.handlers import error_pages
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app.config['SECRET_KEY'] = 'mysecretkey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+Migrate(app, db)
+
 
 app.register_blueprint(core)
 app.register_blueprint(error_pages)
---
+++ BlogPostCompanyProject/puppycompanyblog/models.py
@@ -0,0 +1,32 @@
+from puppycompanyblog import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+class User(db.Model, UserMixin):
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    profile_image = db.Column(db.String(64), nullable=False, default='default_profile.png')
+    email = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+
+    posts = db.relationship('BlogPost', backref='author', lazy=True)
+
+    def __init__(self, email, username, password) -> None:
+        self.email = email
+        self.username = username
+        self.password_hash = generate_password_hash(password)
+
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+    def __repr__(self) -> str:
+        return f"USername {self.username}"
+
+
+class BlogPost():
+    pass
