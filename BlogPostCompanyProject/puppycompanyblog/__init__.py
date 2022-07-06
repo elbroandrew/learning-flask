f8eaca91eb81be08460c2f128bcdfe12e4b5c710
andrew <elbroandrew@gmail.com>
2022-07-06 19:25:48 +1100

    login manager

--- BlogPostCompanyProject/puppycompanyblog/__init__.py
+++ BlogPostCompanyProject/puppycompanyblog/__init__.py
@@ -4,6 +4,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
 from flask_migrate import Migrate
+from flask_login import LoginManager
+
 
 app = Flask(__name__)
 
@@ -18,6 +20,14 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 Migrate(app, db)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'users.login'
+
+
+from puppycompanyblog.core.views import core
+from puppycompanyblog.error_pages.handlers import error_pages
 
 
 app.register_blueprint(core)
--- BlogPostCompanyProject/puppycompanyblog/models.py
+++ BlogPostCompanyProject/puppycompanyblog/models.py
@@ -1,4 +1,4 @@
-from puppycompanyblog import db
+from puppycompanyblog import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
