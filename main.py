d329e83d9593387deab2cf70bf25f03c614ea97a
elbroandrew <elbroandrew@gmail.com>
2022-08-20 18:40:32 +1000

    set cookie with make_response

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, g as app_ctx, redirect
+from flask import Flask, render_template, request, jsonify, g as app_ctx, redirect, session, make_response
 import time
 
 app = Flask(__name__)
@@ -11,18 +11,21 @@ def start_time():
 def count_request_time(response):
     total_time = time.perf_counter() - app_ctx.start_time
     time_in_ms = int(total_time * 1000)
-    print(f"REQUEST TIME: {total_time}")
     return response
 
 @app.route("/")
 def index():
     time.sleep(1)
-    return redirect("/page1", code=302)
+    return "Hello"
 
 
 @app.route("/page1")
 def page():
-    return "PAGE_1"
+    res = make_response("SETTING a COOKIE")
+    res.set_cookie('foo', 'bar', max_age=60*60)
+    #session['my_text'] = 123
+    #return f"{session.get('my_text')}"
+    return res
 
 
 
