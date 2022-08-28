e151d904d685ef04261bc55e5601b4e88ea35635
andrew <elbroandrew@gmail.com>
2022-08-28 15:02:02 +1100

    remove all for pytest project

--- BlueprintProjectStructure/myproject/owners/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField
-
-class AddOwnerForm(FlaskForm):
-    name = StringField('Name of Owner')
-    pup_id = IntegerField('ID of Puppy: ')
-    submit = SubmitField("Add Owner")
