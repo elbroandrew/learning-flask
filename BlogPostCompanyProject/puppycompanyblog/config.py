26339c1141112d618f58b7d62e80db84b43be651
elbroandrew <elbroandrew@gmail.com>
2022-11-28 11:53:05 +1000

    create config py

--- BlogPostCompanyProject/puppycompanyblog/config.py
+++ BlogPostCompanyProject/puppycompanyblog/config.py
@@ -1,2 +1,27 @@
+import os.path
+
 import yaml
 
+class Config:
+
+    __slots__ = [
+        "_path",
+        "DEBUG"
+    ]
+
+    def __init__(self, yaml_file_path: str) -> None:
+        self._path = yaml_file_path
+        self._read()
+
+    def _read(self) -> None:
+        if not os.path.exists(self._path):
+            raise AttributeError(f"yaml config doesn't exist: {self._path}")
+
+        with open(self._path) as config_file:
+            config_content = config_file.read()
+            config_yaml = yaml.safe_load(config_content)
+
+        for k, v in config_yaml.items():
+            k = k.upper()
+            if k in self.__slots__:
+                setattr(self, k, v)
